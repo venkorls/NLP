43529ce29b1e84f42e3ba05762f9e2493f146e17
venkorls <qq715139533@gmail.com>
2018-05-03 16:10:29 +0800

    init

--- common/FileOp.py
+++ common/FileOp.py
@@ -1,5 +1,6 @@
 import os
 
+
 def findFile(file_path, file_type):
     """递归查找指定文件类型的文件列表"""
     file_list = []
@@ -13,3 +14,13 @@ def findFile(file_path, file_type):
                     abspath = os.path.join(top, filename)
                     file_list.append(abspath)
     return file_list
+
+
+def sougou_to_dict(input_file_path, out_dict_path):
+    """将搜狗导出的词汇txt文件转为结巴分词的格式"""
+    inputlines = open(input_file_path, mode='r').readlines()
+    outlines = open(out_dict_path, mode='a+', encoding='utf-8')
+    for line in inputlines:
+        outlines.write(line.split(' ')[-1])
+    outlines.close()
+
